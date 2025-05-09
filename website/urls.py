0c35257736f08ae5de60233279dd1fb0486bf8be
Dmitry Karakulov <karakulovdn@gmail.com>
2025-05-09 15:33:28 +0700

    ДЗ_24

--- website/urls.py
+++ website/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from pages import views
+from pages.views import product_detail
+from pages.views import add_product
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,6 +12,8 @@ urlpatterns = [
     path('catalog/', views.catalog, name='catalog'),
     path('category/', views.category, name='category'),
     path('contacts/', views.contacts, name='contacts'),
+    path('product/<int:pk>/', product_detail, name='product_detail'),
+    path('add-product/', add_product, name='add_product'),
 ]
 
 if settings.DEBUG:
